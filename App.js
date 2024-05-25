2c387e53952d606e506ef0b06e954918eecc328f
level237 <Bramslevel129@gmail.com>
2024-05-25 11:08:18 +0100

    Using Flexbox To Create Layouts

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View,Button, TextInput } from 'react-native';
 export default function App() {
   return (
     <View style={styles.appContainer}>
-       <View>
-      <TextInput placeholder='Your course Goal'/>
+       <View style={styles.inputContainer}>
+      <TextInput style={styles.textInput} placeholder='Your course Goal'/>
       <Button title='Add Goal'/>
     </View>
     <View>
@@ -18,4 +18,14 @@ const styles = StyleSheet.create({
   appContainer: {
    padding:50
   },
+  inputContainer:{
+    flexDirection:'row',
+    justifyContent:"center",
+    gap:16 
+  },
+  textInput:{
+    borderWidth:1,
+    borderColor:"#cccccc",
+    width:'80%',
+  }
 });
