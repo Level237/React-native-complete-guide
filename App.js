1099c69c8df12455d7671b2d4173c794a7107c02
level237 <Bramslevel129@gmail.com>
2024-05-31 17:36:36 +0100

    Setting Up our Screen Components

--- App.js
+++ App.js
@@ -1,70 +1,12 @@
-import { useState } from 'react';
+
 import { StyleSheet, View,FlatList, Button } from 'react-native';
-import GoalItem from './components/GoalItem';
-import GoalInput from './components/GoalInput';
 import { StatusBar } from 'expo-status-bar';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
 
-  const [modalIsVisible,setModalIsVisible]=useState(false)
-  const  [courseGoals,setCourseGoals]=useState([])
-  
-  function startAddGoalHandler(){
-    setModalIsVisible(true)
-  }
-  function endGoalHandler(){
-    setModalIsVisible(false)
-  }
-  function addGoalHandler(enteredGoalText){
-    setCourseGoals(currentCourseGoals=>[...currentCourseGoals,{
-      text:enteredGoalText,id:Math.random().toString()
-    }])
-   endGoalHandler()
-  }
-  function deleteGoalHandler(id){
-    setCourseGoals(currentCourse=>{
-      return currentCourse.filter((goal)=>goal.id!==id)
-    })
-  }
-  return (
-    <>
-    <StatusBar style="light" />
-    <View style={styles.appContainer}>
-        <Button color="#a065ec" onPress={startAddGoalHandler} title='Add New Goal'/>
-       {modalIsVisible  && <GoalInput endModal={endGoalHandler} visible={modalIsVisible} onAddGoal={addGoalHandler} />}
-    <View style={styles.goalsContainer}>
-    <FlatList data={courseGoals} renderItem={itemData=>{
-      return (
-        <GoalItem text={itemData.item.text} onDeleteGoalItem={deleteGoalHandler} id={itemData.item.id}/>
-      )
-    }} 
-    keyExtractor={(item,index)=>{
-      return item.id;
-    }}
-    alwaysBounceVertical={false} />
-    </View>
-    </View>
-    </>
-    
-  );
+
+  return <StartGameScreen/>
 }
 
-const styles = StyleSheet.create({
-  appContainer: {
-    flex:1,
-   paddingTop:50,
-   backgroundColor:"#1e085a",
-   paddingHorizontal:16,
-  },
- 
-  goalsContainer:{
-    flex:5
-  },
-  goalItem:{
-    margin:8,
-    borderRadius:6,
-    backgroundColor:'#5e0acc',
-    padding:8,
-  },
- 
-});
+const styles = StyleSheet.create({});
---
+++ components/PrimaryButton.js
@@ -0,0 +1,9 @@
+import { View,TextInput, Text } from "react-native"
+
+export default function PrimaryButton({children}) {
+  return (
+  <View>
+    <Text>{children}</Text>
+  </View>
+  )
+}
