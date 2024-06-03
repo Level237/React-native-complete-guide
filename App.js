c90c26efe1b9580a81cc8d2434afd9f42cef71ec
level237 <Bramslevel129@gmail.com>
2024-06-03 17:48:24 +0100

    Adding & Using Custom Fonts with React Native Apps with expo-splash

--- App.js
+++ App.js
@@ -2,25 +2,31 @@
 import { StyleSheet,ImageBackground,SafeAreaView} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import StartGameScreen from './screens/StartGameScreen';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import GameScreen from './screens/GameScreen';
 import Colors from './constants/colors';
 import GameOverScreen from './screens/GameOverScreen';
 import { useFonts } from 'expo-font';
-import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 export default function App() {
 
   const [useNumber,setUserNumber]=useState('')
   const [gameIsOver,setGameIsOver]=useState(true)
+  const [fontsLoaded,fontError]=useFonts({
+    "open-sans":require('./assets/fonts/OpenSans-Regular.ttf'),
+    "open-sans-bold":require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+  
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
 
-
-  const [fontsLoaded] =useFonts({
-    'open-sans':require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold':require('./assets/fonts/OpenSans-Bold.ttf')
-  });
-
-  if(!fontsLoaded){
-    return <AppLoading/>;
+  if (!fontsLoaded && !fontError) {
+    return null;
   }
   function pickedNumberHandler(pickedNumber){
     setUserNumber(pickedNumber)
@@ -39,7 +45,7 @@ export default function App() {
     screen=<GameOverScreen/>
   }
 
-  return( <LinearGradient colors={[Colors.primary700,Colors.accent500]} style={styles.rootScreen}>
+  return( <LinearGradient onLayout={onLayoutRootView} colors={[Colors.primary700,Colors.accent500]} style={styles.rootScreen}>
 <ImageBackground 
 source={require('./assets/images/background.png')} 
 resizeMode='cover' 
