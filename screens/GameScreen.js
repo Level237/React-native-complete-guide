6fff77f6f87a7ad6a541e58231c105e8b2775097
level237 <Bramslevel129@gmail.com>
2024-06-03 16:49:34 +0100

    Working with Icons (Button Icons)

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -5,7 +5,7 @@ import NumberContainer from "../components/game/NumberContainer";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Card from "../components/ui/Card";
 import InstructionText from "../components/ui/InstructionText";
-
+import {Ionicons} from "@expo/vector-icons"
 function generateRandomBetween(min, max, exclude) {
     const rndNum = Math.floor(Math.random() * (max - min)) + min;
   
@@ -51,11 +51,15 @@ export default function GameScreen({userNumber,onGameOver}) {
 <InstructionText style={styles.instructionText}>Higher or lower?</InstructionText>
 <View style={styles.alignBtn}>
   <View style={styles.buttonContainer}>
-  <PrimaryButton onPress={nextGuestHandler.bind(this,'lower')}>-</PrimaryButton>
+  <PrimaryButton onPress={nextGuestHandler.bind(this,'lower')}>
+    <Ionicons name="remove" size={24} color="white" />
+  </PrimaryButton>
   </View>
 
   <View style={styles.buttonContainer}>
-  <PrimaryButton onPress={nextGuestHandler.bind(this,'greater')}>+</PrimaryButton>
+  <PrimaryButton onPress={nextGuestHandler.bind(this,'greater')}>
+  <Ionicons name="add" size={24} color="white" />
+  </PrimaryButton>
   </View>
 
 
