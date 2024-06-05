3b63835211cb4f6889b29e832611b705f260f1f5
level237 <Bramslevel129@gmail.com>
2024-06-05 14:21:28 +0100

    Managing Screen Content with KeyboardAvoidingView

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,4 +1,4 @@
-import {TextInput, View,StyleSheet,Alert,useWindowDimensions} from 'react-native'
+import {TextInput, View,StyleSheet,Alert,useWindowDimensions,KeyboardAvoidingView,ScrollView} from 'react-native'
 import { useState } from 'react'
 import PrimaryButton from '../components/ui/PrimaryButton'
 import Colors from '../constants/colors'
@@ -31,7 +31,9 @@ export default function StartGameScreen({onPickNumber}) {
 
     const marginTopDistance =height < 380 ? 30 :100;
   return (
-    <View style={[styles.rootContainer,{marginTop:marginTopDistance}]}>
+    <ScrollView style={styles.screen}>
+  <KeyboardAvoidingView style={styles.screen} behavior='position'>
+         <View style={[styles.rootContainer,{marginTop:marginTopDistance}]}>
         <Title>Guess My Number</Title>
     <Card>
         <InstructionText>Enter a number</InstructionText>
@@ -53,6 +55,11 @@ export default function StartGameScreen({onPickNumber}) {
       </Card>
     
       </View>
+    </KeyboardAvoidingView>
+
+    </ScrollView>
+  
+   
   )
  
   
@@ -60,6 +67,9 @@ export default function StartGameScreen({onPickNumber}) {
 
 //const deviceHeight=Dimensions.get('window').height
 const styles=StyleSheet.create({
+    screen:{
+        flex:1,
+    },
     rootContainer:{
         flex:1,
         //marginTop:deviceHeight < 380 ? 30 :100,
