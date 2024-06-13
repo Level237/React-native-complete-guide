926c9590df1fa7365440903ea13552f16dc508cc
level237 <Bramslevel129@gmail.com>
2024-06-13 22:40:45 +0100

    update

--- navigation/App.js
+++ navigation/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,View } from 'react-native';
 import CategoryScreen from './screens/CategoryScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
