995cb242003af4eccb0b01735fdc50f27c84e964
level237 <Bramslevel129@gmail.com>
2024-06-28 20:50:15 +0100

    Configuring the form input elements

--- expense-track/components/ManageExpense/ExpenseForm.js
+++ expense-track/components/ManageExpense/ExpenseForm.js
@@ -24,7 +24,9 @@ export default function ExpenseForm() {
      />
      <Input label="Description"
      textInputConfig={{ 
-        
+        multiline:true,
+        //autoCapitalize:'none',
+        //autoCorrect:false
       }}
      />
     </View>
