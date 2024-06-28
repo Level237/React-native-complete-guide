cf466a0b859d7e8be65552e3b0c6e9ca821519c4
level237 <Bramslevel129@gmail.com>
2024-06-28 14:00:12 +0100

    Using context from inside component

--- expense-track/screens/AllExpensesScreen.js
+++ expense-track/screens/AllExpensesScreen.js
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useContext } from 'react'
 import ExpenseOutput from '../components/Expenses/ExpenseOutput'
+import { ExpensesContext } from '../store/expense-context'
 
 export default function AllExpensesScreen() {
+
+  const expensesCtx=useContext(ExpensesContext)
   return (
     <View>
-      <ExpenseOutput expensesPeriod="Total"/>
+      <ExpenseOutput expenses={expensesCtx} expensesPeriod="Total"/>
     </View>
   )
 }
--- expense-track/screens/RecentExpenseScreen.js
+++ expense-track/screens/RecentExpenseScreen.js
@@ -1,11 +1,20 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useContext } from 'react'
 import ExpenseOutput from '../components/Expenses/ExpenseOutput'
+import { ExpensesContext } from '../store/expense-context'
+import { getDateMinusDays } from '../utils/date'
 
 export default function RecentExpenseScreen() {
+  const expenseCtx=useContext(ExpensesContext)
+  const recentExpenses=expenseCtx.expenses.filter((expense)=>{
+    const today=new Date();
+    const date7DaysAgo=getDateMinusDays(today,7);
+
+    return expense.date >date7DaysAgo;
+  })
   return (
     <View>
-       <ExpenseOutput expensesPeriod="Last 7 days"/>
+       <ExpenseOutput expenses={recentExpenses} expensesPeriod="Last 7 days"/>
     </View>
   )
 }
