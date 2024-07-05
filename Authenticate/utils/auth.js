dfe78fc6955e62d5e3dab4ae33f618eebe67d2e8
level237 <Bramslevel129@gmail.com>
2024-07-05 23:49:51 +0100

    Sending Authentication requests to the backend

--- Authenticate/utils/auth.js
+++ Authenticate/utils/auth.js
@@ -2,10 +2,12 @@ import axios from "axios"
 
 const API_KEY="AIzaSyBlTy2_8WA3aOlSj9jkQJEs2qa2j7RZkyQ"
 async function createUser(email,password){
-    await axios.post("https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key="+API_KEY,
+    const response=await axios.post("https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key="+API_KEY,
     {
         email:email,
         password:password,
         returnSecureToken:true
-    })
+    });
+
+    
 }
