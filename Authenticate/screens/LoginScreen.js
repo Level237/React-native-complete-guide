15081a819944b70eec75d41b879486753c8c9d2e
level237 <Bramslevel129@gmail.com>
2024-07-06 14:14:20 +0100

    Logging user

--- Authenticate/screens/LoginScreen.js
+++ Authenticate/screens/LoginScreen.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { login } from '../utils/auth';
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticated,setIsAuthenticated]=useState(false)
+  async function loginHandler({email,password}){
+    setIsAuthenticated(true)
+    await login(email,password)
+    setIsAuthenticated(false)
+  }
+
+  if(isAuthenticated){
+    return <LoadingOverlay message="Logging in..."/>
+  }
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
--- Authenticate/utils/auth.js
+++ Authenticate/utils/auth.js
@@ -1,12 +1,21 @@
 import axios from "axios"
 
 const API_KEY="AIzaSyBlTy2_8WA3aOlSj9jkQJEs2qa2j7RZkyQ"
- export async function createUser(email,password){
-    const response=await axios.post("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key="+API_KEY,
-    {
+
+ async function Authenticate(mode,email,password){
+    
+    const url=`https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`
+    const response=await axios.post(url,{
         email:email,
         password:password,
         returnSecureToken:true
-    });
+    })
+    console.log(response.data)
+}
+ export async function createUser(email,password){
+    await Authenticate("signUp",email,password)
+}
 
+export async function login(email,password){
+    await Authenticate("signInWithPassword",email,password)
 }
