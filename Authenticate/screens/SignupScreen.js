1560b2d83a2740258a2c0034f292bd12f1e2913a
level237 <Bramslevel129@gmail.com>
2024-07-06 13:57:24 +0100

    Create new user

--- Authenticate/screens/SignupScreen.js
+++ Authenticate/screens/SignupScreen.js
@@ -1,7 +1,21 @@
+import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
+import { createUser } from '../utils/auth';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
 
 function SignupScreen() {
-  return <AuthContent />;
+
+  const [isAuthenticated,setIsAuthenticated]=useState(false)
+  async function signupHandler({email,password}){
+    setIsAuthenticated(true)
+    await createUser(email,password)
+    setIsAuthenticated(false)
+  }
+
+  if(isAuthenticated){
+    return <LoadingOverlay message="Creating user..."/>
+  }
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
--- Authenticate/utils/auth.js
+++ Authenticate/utils/auth.js
@@ -1,13 +1,12 @@
 import axios from "axios"
 
 const API_KEY="AIzaSyBlTy2_8WA3aOlSj9jkQJEs2qa2j7RZkyQ"
-async function createUser(email,password){
-    const response=await axios.post("https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key="+API_KEY,
+ export async function createUser(email,password){
+    const response=await axios.post("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key="+API_KEY,
     {
         email:email,
         password:password,
         returnSecureToken:true
     });
 
-    
 }
