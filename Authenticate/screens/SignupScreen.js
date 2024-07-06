16f6f5ccab03e2463a0f6758ebc14cb4b57fe4d3
level237 <Bramslevel129@gmail.com>
2024-07-06 14:30:19 +0100

    Authentication Error handling

--- Authenticate/screens/SignupScreen.js
+++ Authenticate/screens/SignupScreen.js
@@ -2,13 +2,18 @@ import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
 import { createUser } from '../utils/auth';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { Alert } from 'react-native';
 
 function SignupScreen() {
 
   const [isAuthenticated,setIsAuthenticated]=useState(false)
   async function signupHandler({email,password}){
     setIsAuthenticated(true)
-    await createUser(email,password)
+    try {
+      await createUser(email,password)
+    } catch (error) {
+      Alert.alert('Authentication failed!','Could not create user.Please check your credentials or try again later.')
+    }
     setIsAuthenticated(false)
   }
 
