c28d07a2e23f4b96957e9cb165390580458af432
level237 <Bramslevel129@gmail.com>
2024-07-14 15:03:43 +0100

    Editing the favorite place items

--- native-device/components/Places/PlaceList.js
+++ native-device/components/Places/PlaceList.js
@@ -1,10 +1,30 @@
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
 import React from 'react'
+import PlaceItem from './PlaceItem'
 
 export default function PlaceList({places}) {
+
+    if(!places || places.length ===0){
+        return <View style={styles.fallBackContainer}>
+            <Text style={styles.fallBackText}>No places added yet -- start adding some!</Text>
+        </View>
+    }
   return (
     <View>
-     <FlatList data={places} keyExtractor={(item)=>item.id} renderItem={d}/>
+     <FlatList data={places} keyExtractor={(item)=>item.id} renderItem={({item})=>{
+        <PlaceItem place={item}/>
+     }}/>
     </View>
   )
 }
+
+const styles=StyleSheet.create({
+    fallBackContainer:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    fallBackText:{
+        color:'white'
+    }
+})
--- native-device/screens/AllPlacesScreen.js
+++ native-device/screens/AllPlacesScreen.js
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import PlaceList from '../components/Places/PlaceList'
 
 export default function AllPlacesScreen() {
   return (
     <View>
-      <Text>AllPlacesScreen</Text>
+      <PlaceList/>
     </View>
   )
 }
